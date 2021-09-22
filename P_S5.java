ccbbd8a325b5fd33f80cd41af3eb6d26c71ffa4a
Pavan Kumar Reddy <51886446+pavanreddy777999@users.noreply.github.com>
2021-09-22 12:37:58 +0530

    Update P_S5.java

--- P_S5.java
+++ P_S5.java
@@ -1,29 +1,20 @@
 import java.util.*;
+
 public class Main
 {
-	public static void main(String[] args) {
-	    Scanner sc=new Scanner(System.in);
-	    System.out.println("Enter the Number");
-	    int n=sc.nextInt();
-	    char[] str=String.valueOf(n).toCharArray();
-	    int len=str.length;
-	    if(n==0)
-	    System.out.println(n);
-	    else if(n>0)
-	    {
-	        for(int i=0;i<len;i++)
-	          {
-	             System.out.print(str[i]);
-	             System.out.print(str[i]);
-	           }
-	    }
-	    else {
-	        System.out.print("-");
-	        for(int i=1;i<len;i++)
-	          {
-	             System.out.print(str[i]);
-	             System.out.print(str[i]);
-	          }
-	    }
-	}
+    public static Integer doubleDigit(int n)
+    {
+         if (n == 0)
+            return 0;
+        else
+        return doubleDigit (n/ 10) * 100 + (n % 10) * 10 + n % 10;
+        
+        //return 0;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int n=sc.nextInt();
+        System.out.println(doubleDigit(n));
+    }
 }
