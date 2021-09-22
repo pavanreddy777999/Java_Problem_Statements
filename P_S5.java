befabdb9f82c8e009431b983c874fa17df023f07
Pavan Kumar Reddy <51886446+pavanreddy777999@users.noreply.github.com>
2021-09-22 15:22:47 +0530

    Update P_S5.java

--- P_S5.java
+++ P_S5.java
@@ -1,13 +1,13 @@
-import java.util.*;
+ import java.util.*;
 
 public class Main
 {
-    public static Integer doubleDigit(int n)
+    public static Integer ps5(int n)
     {
          if (n == 0)
             return 0;
         else
-        return doubleDigit (n/ 10) * 100 + (n % 10) * 10 + n % 10;
+        return ps5 (n/ 10) * 100 + (n % 10) * 10 + n % 10;
         
         //return 0;
     }
@@ -15,6 +15,45 @@ public class Main
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the Number");
         int n=sc.nextInt();
-        System.out.println(doubleDigit(n));
+        System.out.println(ps5(n));
     }
-}
+} 
+
+/* without recursion */
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int n=sc.nextInt();
+        String s=Integer.toString(n);
+        int len=s.length();
+        if(n<0)
+        {
+            if(len%2==0)
+            len-=1;
+        }
+        char[] c=s.toCharArray();
+        if(len%2==0)
+        {
+            for(int i=0;i<len;i++)
+            {
+                System.out.print(c[i]);
+                System.out.print(c[i]);
+            }
+        }
+        else 
+        {
+           System.out.print("-");
+            for(int i=1;i<=len;i++)
+            {
+                System.out.print(c[i]);
+                System.out.print(c[i]);
+            }
+            
+        }
+        System.out.println();
+    }}
+
